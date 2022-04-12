793391cfaabcbfb015868046a1092f93dc8f9bd3
julia <juliabadita@live.fr>
2022-04-12 22:02:07 +0200

    adding the social medias in the footer and blank links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from '../assets/logo_light-mode.png'
+import facebook from '../assets/facebook.svg'
+import instagram from '../assets/instagram.svg'
+import youtube from '../assets/youtube.svg'
 import '../stylesheets/main.scss'
 
 const Footer = () => {
@@ -7,9 +11,31 @@ const Footer = () => {
     <>
       <footer>
         <article class="footer">
-          <img src={logo} className="footer__logo" alt="logo académie Jedi" />©
-          2022 Académie Jedi Team | All rights reserved
-          <div class="footer__social-medias"></div>
+          <img src={logo} className="footer__logo" alt="logo académie Jedi" />
+          <div className="footer__social-medias">
+            <Link to="/">
+              <img
+                src={facebook}
+                className="social-medias__logo"
+                alt="Lien vers notre Facebook"
+              />
+            </Link>
+            <Link to="/">
+              <img
+                src={instagram}
+                className="social-medias__logo"
+                alt="Lien vers notre Instagram"
+              />
+            </Link>
+            <Link to="/">
+              <img
+                src={youtube}
+                className="social-medias__logo"
+                alt="Lien vers notre Youtube"
+              />
+            </Link>
+          </div>
+          © 2022 Académie Jedi Team | All rights reserved
         </article>
       </footer>
     </>

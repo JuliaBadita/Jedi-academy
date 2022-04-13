d033a6f2d241bd4e3bb75d018ead6b34d8abf388
julia <juliabadita@live.fr>
2022-04-13 12:15:27 +0200

    modifying details in the navbar in resp mode, adding Lorem content to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,15 @@ const Footer = () => {
   return (
     <>
       <footer>
+        <article>
+          <h4>À propos</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quae
+            sed perferendis eius molestiae explicabo itaque quasi nesciunt
+            commodi qui! Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Necessitatibus, labore.
+          </p>
+        </article>
         <article class="footer">
           <img src={logo} className="footer__logo" alt="logo académie Jedi" />
           <div className="footer__social-medias">
@@ -36,6 +45,17 @@ const Footer = () => {
             </Link>
           </div>
           © 2022 Académie Jedi Team | All rights reserved
+        </article>
+        <article>
+          <h4>Liens utiles</h4>
+          <Link to="/"> Site officiel Star Wars</Link>
+          <Link to="/">Boutique Sabre Galactique</Link>
+          <Link to="/">Conditions d'utilisations</Link>
+        </article>
+        <article>
+          <h4>Rejoignez l'aventure !</h4>
+          <p>jedi-academie@padawan.fr</p>
+          <p>0612345678</p>
         </article>
       </footer>
     </>

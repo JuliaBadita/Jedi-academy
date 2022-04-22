4354b6228729575eb0385f60bccfea73e7428f31
julia <juliabadita@live.fr>
2022-04-22 16:21:00 +0200

    adding the animation to the history page

--- src/components/History.js
+++ src/components/History.js
@@ -3,48 +3,62 @@ import React from 'react'
 const History = () => {
   return (
     /// History animated
-    <div className="perspective">
-      <article className="history__animated">
-        <h1>EPISODE I</h1>
-        <h2>L'Académie Jedi</h2>
-        <br />
-        <p>
-          <h3>Le combat au Sabre Laser sportif individuel</h3>
+    <>
+      <div className="music__star-wars">
+        <div className="music__sw-container">
+          <embed
+            src="https://www.youtube.com/v/EjMNNpIksaI?version=3&amp;hl=en_US&autoplay=1&amp;autohide=2"
+            type="application/x-shockwave-flash"
+            allowscriptaccess="always"
+            allowfullscreen="true"
+          ></embed>
+        </div>
+      </div>
+
+      <div className=" history__perspective">
+        <article className="history__animated">
+          <h1>EPISODE I</h1>
+          <h2>L'Académie Jedi</h2>
           <br />
-          Ces compétitions de type duel, ou « combat en individuel », sont des
-          affrontements successifs entre deux pratiquants. Appelés également
-          combattants, équipés d’arme et de protections similaires. À la fin de
-          la compétition un classement des protagonistes est réalisé. Le but est
-          de toucher son partenaire par une touche, exécutée avec le tranchant
-          de son sabre laser. Tout en évitant d’être touché et en restant dans
-          la zone d’évolution. ​<br /> <br />
-          <h3>Le Combat au Sabre Laser chorégraphié </h3>
-          <br />
-          Les combats chorégraphiés laissent les pratiquants créer leur propre
-          chorégraphie de combat. Un certain nombre de mouvements imposés
-          doivent être intégrés, ils peuvent servir de critères de jugement
-          communs lors de tournois. Ils permettent aux pratiquants de rapidement
-          s’immerger dans des combats aux sabres laser en coopération et de
-          manière sécurisé <br />​<h3>Les katas techniques au Sabre Laser </h3>
-          <br />
-          Ces Katas permettent aux pratiquants de perfectionner leur maîtrise de
-          l’arme emblématique de l’univers créé par George Lucas. Ce sont des
-          mouvements codifiés qui s’enchaînent pour former un combat imaginaire
-          contre un ou plusieurs adversaires. Ils peuvent être réalisés seul ou
-          à deux. Chaque kata a des spécificités techniques liés à la Forme de
-          combat au sabre laser présentée.
-          <br />
-          <br />
-          <h3>Le combat au Sabre Laser en équipe</h3>
-          <br />
-          Le Tournoi de Combats en Équipe oppose un groupe de combattants contre
-          un autre groupe de combattants, successivement, pour essayer de
-          déterminer la meilleure équipe. Ces équipes sont affiliées au Côté
-          Lumineux avec des Chevalier de la Lumière, ou au Côté Obscur avec des
-          Guerriers des Ténèbres.
-        </p>
-      </article>
-    </div>
+          <p>
+            <h3>Le combat au Sabre Laser sportif individuel</h3>
+            <br />
+            Ces compétitions de type duel, ou « combat en individuel », sont des
+            affrontements successifs entre deux pratiquants. Appelés également
+            combattants, équipés d’arme et de protections similaires. À la fin
+            de la compétition un classement des protagonistes est réalisé. Le
+            but est de toucher son partenaire par une touche, exécutée avec le
+            tranchant de son sabre laser. Tout en évitant d’être touché et en
+            restant dans la zone d’évolution. ​<br /> <br />
+            <h3>Le Combat au Sabre Laser chorégraphié </h3>
+            <br />
+            Les combats chorégraphiés laissent les pratiquants créer leur propre
+            chorégraphie de combat. Un certain nombre de mouvements imposés
+            doivent être intégrés, ils peuvent servir de critères de jugement
+            communs lors de tournois. Ils permettent aux pratiquants de
+            rapidement s’immerger dans des combats aux sabres laser en
+            coopération et de manière sécurisé <br />​
+            <h3>Les katas techniques au Sabre Laser </h3>
+            <br />
+            Ces Katas permettent aux pratiquants de perfectionner leur maîtrise
+            de l’arme emblématique de l’univers créé par George Lucas. Ce sont
+            des mouvements codifiés qui s’enchaînent pour former un combat
+            imaginaire contre un ou plusieurs adversaires. Ils peuvent être
+            réalisés seul ou à deux. Chaque kata a des spécificités techniques
+            liés à la Forme de combat au sabre laser présentée.
+            <br />
+            <br />
+            <h3>Le combat au Sabre Laser en équipe</h3>
+            <br />
+            Le Tournoi de Combats en Équipe oppose un groupe de combattants
+            contre un autre groupe de combattants, successivement, pour essayer
+            de déterminer la meilleure équipe. Ces équipes sont affiliées au
+            Côté Lumineux avec des Chevalier de la Lumière, ou au Côté Obscur
+            avec des Guerriers des Ténèbres.
+          </p>
+        </article>
+      </div>
+    </>
   )
 }
 
